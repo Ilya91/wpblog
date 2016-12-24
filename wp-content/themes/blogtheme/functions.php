5c72339518e6c4e12cfcc7eef8e39f6e146f6cb1
Ilya91 <webmaster.1991@mail.ru>
2016-12-24 17:31:05 +0300

    function register_menu()

--- wp-content/themes/blogtheme/functions.php
+++ wp-content/themes/blogtheme/functions.php
@@ -16,3 +16,27 @@ function add_theme_scripts() {
 
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+/*function change_title($content){
+    $new_content = $content . ' - new addition';
+    return $new_content;
+}
+add_filter('the_title', 'change_title');*/
+
+/*function change_content($content){
+    return $content = 'Hello world';
+}
+add_filter('default_content', 'change_content');*/
+
+function author_register($name){
+    return ucwords($name);
+}
+add_filter('the_author', 'author_register');
+
+/**
+ * Activate nav menu
+ */
+function register_menu(){
+    register_nav_menu('primary', 'Primary navigation menu');
+}
+add_action( 'after_setup_theme', 'register_menu' );
